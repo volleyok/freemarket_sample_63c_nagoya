6ef3713624390f1dd401e13a87a7e277c5ae649e
KeiFunahashi <fnhskei@yahoo.co.jp>
2019-11-22 11:25:08 +0900

    データベース編集

--- db/migrate/20191120090215_create_posts.rb
+++ db/migrate/20191120090215_create_posts.rb
@@ -1,8 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.string        :name
+      t.string      :name
+      t.string      :image
       t.text        :description
+      t.string      :cotegory
+      t.string      :status
+      t.string      :postage
+      t.string      :ship_from
+      t.string      :shipping_date
+      t.integer     :price
       t.timestamps
     end
   end
