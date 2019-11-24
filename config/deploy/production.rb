dedf047e60e5aefe04a686e4a43a6e6d20f84639
volleyok <56710628+volleyok@users.noreply.github.com>
2019-11-24 11:01:32 +0900

    Basic認証の実装

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
 server '18.182.255.29', user: 'ec2-user', roles: %w{app db web}
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
