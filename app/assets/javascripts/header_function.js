24408e0dfe6aa02551c2d0bce23a1bca5f35f66c
KeiFunahashi <fnhskei@yahoo.co.jp>
2019-11-25 11:37:19 +0900

    検索プルダウン作成

---
+++ app/assets/javascripts/header_function.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.left-contents li').hover(function() {
+        $(".category-lists:not(:animated)", this).slideToggle(0);
+        $(".brand-lists:not(:animated)", this).slideToggle(0);
+    });
+
+    // $('.ladies').hover(function() {
+    //     console.log('ok')
+    //         $(".ladies-lists", this).animate({ width: 'toggle' }, 0);
+
+    // });
+
+
+
+});
