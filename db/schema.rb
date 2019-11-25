218acd69da9f95e307224c58e6a5fd5c5ee56fd2
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2019-11-25 17:49:55 +0900

    変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191125025006) do
+ActiveRecord::Schema.define(version: 20191125045204) do
 
   create_table "buy_orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "saler_id"
