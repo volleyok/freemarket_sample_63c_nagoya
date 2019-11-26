7a8030165fae74ee5faec3e02ee97e26e352f46a
morigorikun <morigorikun@iCloud.com>
2019-11-26 14:54:36 +0900

    controller変更
    変更

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,32 +1,26 @@
 class SignupsController < ApplicationController
-
+  before_action :set_user, only: [:create, :new, :phone, :address, :check, :complete ]
 
   def index
     
   end
 
-  def create
-    @user = User.new
+  def create,
   end
 
   def new
-    @user = User.new
   end
 
   def phone
-    @user = User.new
   end
 
   def address
-    @user = User.new
   end
 
   def check
-    @user = User.new
   end
 
   def complete
-    @user = User.new
   end
 
   def done
@@ -39,6 +33,10 @@ class SignupsController < ApplicationController
       params.require(:user).permit(
 
       )     
+    end
+
+    def set_user
+      @user = User.new
     end
 
 
