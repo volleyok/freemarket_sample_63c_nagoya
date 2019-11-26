ea9ae9d987908f4f9d974138c9dab70762edaf60
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2019-11-26 19:12:54 +0900

    ログアウト画面

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,6 +3,19 @@ class MypagesController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def delete
+    @user = User.find(current_user.id)
+  end
+
   def show
     @post = Post.where(user_id: current_user.id)
   end
