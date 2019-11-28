424a48220342e243c65d605980194c338e263f29
junki0826 <junki19940826@gmail.com>
2019-11-28 19:52:05 +0900

    画像投稿編集

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', ()=> {
+  var images = [];
   $(function(){
     $('form').on('change', 'input[type="file"]','#image' ,function(e) {
       var file = e.target.files[0],
@@ -18,10 +19,26 @@ $(document).on('turbolinks:load', ()=> {
           }));
         };
       })(file);
+      reader.onload =(function(e){
+        var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>');
+        $preview.append($(btn_wrapper).attr({
+
+        }));
+      });
       reader.readAsDataURL(file);
-      var new_image = $(`<input multiple= "multiple" name="product_images[image][]"  type="file" id="image" data: {image:*}>`);
+      images.push('<img>');
+      var new_image = $(`<input multiple= "multiple" name="product_images[image][]"  type="file" id="image" image =${images.length} style = display:none>`);
       $(".image_box").prepend(new_image);
     });
+    // $(document).on('click', '.delete',".preview",function() {
+    //   var file = e.target.files[0],
+    //   reader = new FileReader(),
+    //   t = this;
+      
+      
+        
+      
+    // });
   });
 });
 
@@ -59,50 +76,3 @@ $(document).on('turbolinks:load', ()=> {
 
 
 
-// $(document).on('turbolinks:load', ()=> {
-//   var new_image = $(`<input multiple= "multiple" name="product_images[image][]"  type="file" id="image">`);
-//   $(".image_box").prepend(new_image);
-//   const buildFileField = (post)=> {
-//     const html = `<div data-index="${post}" class="js-file_group">
-//                     <input class="js-file" type="file"
-//                     name="product[images_attributes][${post}][src]"
-//                     id="#image${post}"><br>
-//                     <div class="js-remove">削除</div>
-//                   </div>`;
-//     return html;
-//   }
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-//   $(function(){
-//     $('form').on('change', 'input[type="file"]','#image' ,function(e) {
-//       var file = e.target.files[0],
-//       reader = new FileReader(),
-//       $preview = $(".preview");
-//       t = this;
-//       if(file.type.indexOf("image") < 0){
-//         return false;
-//       }
-//       reader.onload = (function(file) {
-//         return function(e) {
-//           // $preview.empty();
-//           $preview.append(buildFileField(fileIndex[0]));
-//           fileIndex.shift();
-//           fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-//           $preview.append($('<img>').attr({
-//             src: e.target.result,
-//             width: "150px",
-//             class: "preview",
-//             title: file.name
-//           }));
-//         };
-//       })(file);
-//       reader.readAsDataURL(file);
-//       // var new_image = $(`<input multiple= "multiple" name="product_images[image][]"  type="file" id="image">`);
-//       // $(".image_box").prepend(new_image);
-//     });
-//     $('form').on('click', ".preview", function() {
-//       $(this).parent().remove();
-//       if ($(".previewa").length == 0) $('.image-box').append(buildFileField(fileIndex[0]));
-//     });
-//   });
-// });
