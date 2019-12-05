40c5a1974c7184b7844d52bf12e9b5985a608587
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2019-12-05 15:33:30 +0900

    スクロール

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,12 +1,16 @@
-$(function(){
-  // #で始まるリンクをクリックしたら実行されます
-  $('a[href^="#"]').click(function() {
-    // スクロールの速度
-    var speed = 400; // ミリ秒で記述
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
-    return false;
+$(document).on('turbolinks:load', ()=> {
+  $(function() {
+    $(".category-style-0, .category-style-1").click(function () {
+        var i = $(".category-style-0, .category-style-1").index(this)
+        var p = $(".bbb").eq(i).offset().top;
+        var speed = 500;
+        $('html,body').animate({ scrollTop: p },speed,);
+        return false;
+    });
+
+    $(".btn_top").click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 'fast');
+        return false;
+    });
   });
 });
