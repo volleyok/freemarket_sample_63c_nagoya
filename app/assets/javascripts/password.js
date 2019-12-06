72226261aabc1376bc494cb86835b7d5cc12f092
morigorikun <morigorikun@iCloud.com>
2019-12-06 15:04:09 +0900

    崩れを直す

--- app/assets/javascripts/password.js
+++ app/assets/javascripts/password.js
@@ -1,14 +1,14 @@
-$(document).on('turbolinks:load', function() {
+// $(document).on('turbolinks:load', function() {
 
-  $(function() {
-    var pw = document.getElementById('password');
-    var pwCheck = document.getElementById('password-check');
-    pwCheck.addEventListener('change', function() {
-        if(pwCheck.checked) {
-            pw.setAttribute('type', 'text');
-        } else {
-            pw.setAttribute('type', 'password');
-        }
-        }, false);
-  });
-});
+//   $(function() {
+//     var pw = document.getElementById('password');
+//     var pwCheck = document.getElementById('password-check');
+//     pwCheck.addEventListener('change', function() {
+//         if(pwCheck.checked) {
+//             pw.setAttribute('type', 'text');
+//         } else {
+//             pw.setAttribute('type', 'password');
+//         }
+//         }, false);
+//   });
+// });
