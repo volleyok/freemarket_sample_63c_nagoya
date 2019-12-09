16755a30c1893a4348640f2eb4dde848fd10a9b2
KeiFunahashi <fnhskei@yahoo.co.jp>
2019-12-09 20:45:03 +0900

    商品詳細ページの条件分岐

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+
+
   belongs_to :seller, class_name: "User", optional: true
   belongs_to :buyer, class_name: "User", optional: true
   belongs_to :brand, dependent: :destroy
