6e09c249c009f8c0b88afb7784ea701db16b65ae
junki0826 <junki19940826@gmail.com>
2019-12-09 12:24:03 +0900

    変更

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,84 +1,75 @@
 $(document).on('turbolinks:load', () => {
-    var images = [];
-    // console.log(images)
+  var images = [];
+  // var inputs  =[];
     $(function() {
-      $('form').on('change', 'input[type="file"]', '#image', function(e) {
+      $('form').on('change', 'input[type="file"]', '#post_images_attributes_0_image_url', function(e) {
         var file = $(this).prop("files")[0],
-        reader = new FileReader(),
-                $preview = $(".preview"),
-                btn_wrapper = $(
-                  // 動的にIDを振る
-                  `<div class="btnimage" id ="btnimages">
-                    <div class="new-btn-edit">
-                    編集
-                    </div>
-                    <div class="new-btn-delete">
-                    削除
-                    </div>
-                  </div>`
-                  )
-            t = this;
-            reader.onload = (function(file) {
-                return function(e) {
-                  //  var image_item = `< div class ="aaa"></div>`
-                  //  ('.preview').append(image_item)
-                  //  image_item.append(btn_wrapper)
-                  $preview.append(btn_wrapper)
-                  $('.btnimage').prepend($('<img>').attr({
-                    src: e.target.result,
-                    width: "110px", 
-                    height: "110px",
-                    class: "preview_image",
-                    title: file.name
-                  }));
-                   
-                  
-                };
-            })(file);
-            reader.readAsDataURL(file);
-            images.push('<img>');
-            var new_image = $(`<input accepts_nested_attributes_for= "accepts_nested_attributes_for"  name="product_images[image][]"  type="file" id="image" image =${images.length} style = display:none>`);
+        reader = new FileReader();
+        $preview = $(".preview");
+        btn_wrapper = $(
+          // 動的にIDを振る
+          `<div class="btnimage" id ="btnimages">
+            <div class="new-btn-delete">削除</div>
+          </div>`
+        );
+                    
+          reader.onload = (function(file) {
+            return function(e) {
+              $preview.append(btn_wrapper);
+              $(".btnimage").prepend($('<img>').attr({
+                src: e.target.result,
+                width: "110px", 
+                height: "110px",
+                margin:"5px",
+              }));
+            };
+          })(file);
+          reader.readAsDataURL(file);
+          images.push('<img>');
+          // inputs.push($(this));
+                    
+            var new_image = $(`<input accepts_nested_attributes_for= "accepts_nested_attributes_for"  name="product_images[image][]"  type="file" id="post_images_attributes_0_image_url" image =${images.length} style = display:none>`);
             $(".image_box").prepend(new_image);
         });
-        // $('#post_images_attributes_0_image_url').on('change', function() {
-        //     $('#post_images_attributes_0_image_url').css('display', 'none');
-        //     $('#post_images_attributes_1_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_1_image_url').on('change', function() {
-        //     $('#post_images_attributes_1_image_url').css('display', 'none');
-        //     $('#post_images_attributes_2_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_2_image_url').on('change', function() {
-        //     $('#post_images_attributes_2_image_url').css('display', 'none');
-        //     $('#post_images_attributes_3_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_3_image_url').on('change', function() {
-        //     $('#post_images_attributes_3_image_url').css('display', 'none');
-        //     $('#post_images_attributes_4_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_4_image_url').on('change', function() {
-        //     $('#post_images_attributes_4_image_url').css('display', 'none');
-        //     $('#post_images_attributes_5_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_5_image_url').on('change', function() {
-        //     $('#post_images_attributes_5_image_url').css('display', 'none');
-        //     $('#post_images_attributes_6_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_6_image_url').on('change', function() {
-        //     $('#post_images_attributes_6_image_url').css('display', 'none');
-        //     $('#post_images_attributes_7_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_7_image_url').on('change', function() {
-        //     $('#post_images_attributes_7_image_url').css('display', 'none');
-        //     $('#post_images_attributes_8_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_8_image_url').on('change', function() {
-        //     $('#post_images_attributes_8_image_url').css('display', 'none');
-        //     $('#post_images_attributes_9_image_url').css('display', 'block');
-        // });
-        // $('#post_images_attributes_9_image_url').on('change', function() {
-        //     $('#post_images_attributes_9_image_url').css('display', 'none');
-        // });
+        $('#post_images_attributes_0_image_url').on('change', function() {
+            $('#post_images_attributes_0_image_url').css('display', 'none');
+            $('#post_images_attributes_1_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_1_image_url').on('change', function() {
+            $('#post_images_attributes_1_image_url').css('display', 'none');
+            $('#post_images_attributes_2_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_2_image_url').on('change', function() {
+            $('#post_images_attributes_2_image_url').css('display', 'none');
+            $('#post_images_attributes_3_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_3_image_url').on('change', function() {
+            $('#post_images_attributes_3_image_url').css('display', 'none');
+            $('#post_images_attributes_4_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_4_image_url').on('change', function() {
+            $('#post_images_attributes_4_image_url').css('display', 'none');
+            $('#post_images_attributes_5_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_5_image_url').on('change', function() {
+            $('#post_images_attributes_5_image_url').css('display', 'none');
+            $('#post_images_attributes_6_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_6_image_url').on('change', function() {
+            $('#post_images_attributes_6_image_url').css('display', 'none');
+            $('#post_images_attributes_7_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_7_image_url').on('change', function() {
+            $('#post_images_attributes_7_image_url').css('display', 'none');
+            $('#post_images_attributes_8_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_8_image_url').on('change', function() {
+            $('#post_images_attributes_8_image_url').css('display', 'none');
+            $('#post_images_attributes_9_image_url').css('display', 'block');
+        });
+        $('#post_images_attributes_9_image_url').on('change', function() {
+            $('#post_images_attributes_9_image_url').css('display', 'none');
+        });
         $('.preview').on('click','.new-btn-delete',function(){
           $(this)
             .parent()
@@ -86,7 +77,6 @@ $(document).on('turbolinks:load', () => {
         });
 
     });
-
 
 
     $(function() {
