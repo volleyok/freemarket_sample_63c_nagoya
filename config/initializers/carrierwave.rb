5142c4e7034a4ba37257652b1865ce76897f8929
volleyok <56710628+volleyok@users.noreply.github.com>
2019-12-10 12:11:24 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,35 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory  = 'mercariteamc'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercariteamc'
-  end
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'ercariteamc'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ercariteamc'
 end
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
+
+# CarrierWave.configure do |config|
+#   if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+#     config.storage = :file
+#   elsif Rails.env.production? #本番はS3に保存する
+#     config.storage = :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
+#       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+#       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+#       region: 'ap-northeast-1'
+#     }
+#     config.fog_directory  = 'mercariteamc'
+#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercariteamc'
+#   end
+# end
