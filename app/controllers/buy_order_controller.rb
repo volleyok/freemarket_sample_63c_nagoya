447c2d84a7fbb718d116eb28be39e848f8e0ad10
junki0826 <junki19940826@gmail.com>
2019-12-10 18:41:08 +0900

    変更

--- app/controllers/buy_order_controller.rb
+++ app/controllers/buy_order_controller.rb
@@ -2,7 +2,7 @@ class BuyOrderController < ApplicationController
   def show
     require "payjp"
     card = Card.where(user_id: current_user.id).first
-    post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     charge = Payjp::Charge.create(
       amount: post.price,
